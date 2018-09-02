c9fc826c9b09469c0cfff202434aeab0f3dfbd59
Oleksandr Pidlisnyi <oleksandr.pidlisnyi@gmail.com>
2018-09-02 11:48:04 +0300

    added minimal React + Webpack + Babel setup

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './main.css';
+
+ReactDOM.render(
+  <div>Hello World</div>,
+  document.getElementById('app'),
+);
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: path.resolve(__dirname, './src/index.js'),//'src/index.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'app.js',
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: { minimize: true }
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    })
+  ]
+};
