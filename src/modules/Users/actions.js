bcabc8ef2f98467baf961d4515fff2cd6f11cd4f
Oleksandr Pidlisnyi <oleksandr.pidlisnyi@gmail.com>
2018-09-11 12:43:16 +0300

    Initial user management changes

---
+++ src/modules/Users/actions.js
@@ -0,0 +1,39 @@
+import {
+  FETCH_USERS,
+  FETCH_USERS_FAILURE,
+  FETCH_USERS_SUCCESS,
+} from './constants';
+
+/**
+ * Fetch users action
+ * @returns {Object}
+ */
+const fetchUsers = () => ({
+  type: FETCH_USERS,
+});
+
+/**
+ * Fetch users failed action
+ * @param {Object} error
+ * @returns {{ type: String, payload: { error } }}
+ */
+const fetchUsersFailure = error => ({
+  type: FETCH_USERS_FAILURE,
+  payload: { error },
+});
+
+/**
+ * Fetch users success action
+ * @param {Array} users
+* @returns {{ type: String, payload: { users } }}
+ */
+const fetchUsersSuccess = users => ({
+  type: FETCH_USERS_SUCCESS,
+  payload: { users },
+});
+
+export {
+  fetchUsers,
+  fetchUsersFailure,
+  fetchUsersSuccess,
+};
---
+++ src/modules/Users/constants.js
@@ -0,0 +1,3 @@
+export const FETCH_USERS = 'app/Users/FETCH_USERS';
+export const FETCH_USERS_FAILURE = 'app/Users/FETCH_USERS_FAILURE';
+export const FETCH_USERS_SUCCESS = 'app/Users/FETCH_USERS_SUCCESS';
