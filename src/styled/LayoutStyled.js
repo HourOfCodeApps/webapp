ef3d43a0e8272ee669daae6a63e60db82e1979fe
Sviatoslav Ohura <oguraslavik@gmail.com>
2018-10-08 22:03:48 +0300

    welcome hero, added styled

--- src/styled/LayoutStyled.js
+++ src/styled/LayoutStyled.js
@@ -1,24 +1,37 @@
 import React from 'react';
 import styled from 'styled-components';
 import Grid from '@material-ui/core/Grid';
+import { colors } from './styles';
 
-export const Container = styled(Grid)`
+export const Container = styled(Grid)` // inherits m-ui props
   max-width: 940px;
-  height: 100vh;
+  height: ${props => props.fullScreen && '100vh'};
+
+  ${props => props.marginCenter && `margin: 0 auto`};  // centering without flex wrapper; 
 `;
 
-export const Column = styled(Grid)`
+export const Column = styled(Grid)` // inherits m-ui props
   position: relative;
   
   ${props => props.withBar && `
     &:before {
       content: "";
       position: absolute;
-      top: -15px;
+      top: -40px;
       left: -75px;
       width: 1px;
       height: 300px;
-      background: #b9b9b9;
+      background-color: ${colors.primary};
+      opacity: .2;
     }
   `}
+`;
+
+export const FlexBox = styled.div`
+  display: flex;
+  flex-direction: ${props => props.column ? 'column' : 'row'};
+  flex-wrap: ${props => props.nowrap ? 'nowrap' : 'wrap'};
+  justify-content: ${props => props.justify};
+  align-items: ${props => props.align};
+  margin: ${props => props.margin && props.margin};
 `;
