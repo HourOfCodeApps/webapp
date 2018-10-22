afe19d89bbc716da3acd5669a16754c83bbd0dde
Oleksandr Pidlisnyi <oleksandr.pidlisnyi@gmail.com>
2018-10-22 18:04:50 +0300

    User profile

--- src/modules/Profile/index.js
+++ src/modules/Profile/index.js
@@ -1,41 +1,10 @@
-// import SignIn from './containers/SignIn';
-// import SignUp from './containers/SignUp';
-// import CompleteSignUp from './containers/CompleteSignUp';
+import container from './container';
 
 import sagas from './sagas';
 import reducer from './reducer';
 
-// import { authStateInit, signOut } from './actions';
-// import {
-//   selectAuth,
-//   selectUser,
-//   selectUserLoading,
-//   selectStateInitLoaded,
-//   selectSigningIn,
-//   selectSigningInError,
-//   selectSigningUp,
-//   selectSigningUpError,
-// } from './selectors';
-
-import container from './container';
-
 export default container;
 export {
-//   authStateInit,
-//   CompleteSignUp,
-//   signOut,
   reducer,
   sagas,
-//   selectAuth,
-//   selectStateInitLoaded,
-//   selectUser,
-//   selectUserLoading,
-//   selectSigningIn,
-//   selectSigningInError,
-//   selectSigningUp,
-//   selectSigningUpError,
-//   SignIn,
-//   SignUp,
-//   withAuth,
-//   withUser,
 };
