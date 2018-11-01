4f6fb398ff57434578b95be9d3b11c04a235148b
Oleksandr Pidlisnyi <oleksandr.pidlisnyi@gmail.com>
2018-11-01 21:17:19 +0200

    wip

--- internals/webpack.resolve.js
+++ internals/webpack.resolve.js
@@ -5,7 +5,7 @@ const dirs = require('./dirs');
 module.exports = {
   resolve: {
     alias: {
-      config: path.resolve(dirs.src, 'config', 'development.js'),
+      config: path.resolve(dirs.src, 'config', process.env.NODE_ENV === 'production' ? 'production.js' : 'development.js'),
     },
     extensions: ['.js', '.jsx'],
     modules: [dirs.src, 'node_modules'],
