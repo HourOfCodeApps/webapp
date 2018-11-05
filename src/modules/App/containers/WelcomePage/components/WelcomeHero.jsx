cfa70d8e1d5ba8fc4a50bbec48480df2e5e80a55
SviatoslavOhura <oguraslavik@gmail.com>
2018-11-06 01:43:24 +0200

    Ui/home updates (#28)
    * Update texts on welcome page
    
    * Added new background, changed styles, fixed links

--- src/modules/App/containers/WelcomePage/components/WelcomeHero.jsx
+++ src/modules/App/containers/WelcomePage/components/WelcomeHero.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import withTheme from '@material-ui/core/styles/withTheme';
 import { Container, FlexBox } from 'shared/components/LayoutStyled';
-import { Heading } from 'shared/components/TypographyStyled';
+import { Heading, HeadingSm } from 'shared/components/TypographyStyled';
 
 import {
   WelcomeHeroWrapper,
@@ -13,18 +13,17 @@ const WelcomeHero = props => (
   <WelcomeHeroWrapper>
     <FlexBox margin="0 0 30px 30px" align="center">
       <Logo />
-      <Heading bolder color={props.theme.palette.common.white}>Lviv</Heading>
+      <HeadingSm bolder color={props.theme.palette.common.white}>Lviv</HeadingSm>
     </FlexBox>
-    <Container alignItems="center" marginCenter>
-      <Heading margin="0 0 25px 0" lineHeight="24px" fontSize="18px">
-        Година Коду є глобальним заходом, який залучає
-        десятки мільйонів учнів
-        <br />
-        у більш, як 180 країнах світу.
+    <Container marginCenter container direction="column" justify="center" minHeight={300}>
+      <Heading margin="0 0 25px 0" color={props.theme.palette.common.white} lineHeight="32px" fontSize="24px">
+        Година коду - це глобальна ініціатива щодо популяризації програмування серед школярів
+        усього світу, метою якої є показати учням, що інформатика може бути веселою та цікавою.
+        637,564,646 учнів вже спробували Годину коду!
       </Heading>
-      <Heading link fontSize="18px">
+      <Heading link fontSize="18px" color={props.theme.palette.common.white}>
         Більше інформації на&nbsp;
-        <span>https://hourofcode.com/ua</span>
+        <span><a href="https://hourofcode.com/ua/uk">https://hourofcode.com/ua/uk</a></span>
       </Heading>
     </Container>
   </WelcomeHeroWrapper>
--- src/shared/components/WelcomePageStyled.js
+++ src/shared/components/WelcomePageStyled.js
@@ -2,28 +2,33 @@ import styled from 'styled-components';
 import withTheme from '@material-ui/core/styles/withTheme';
 
 import LogoImage from 'assets/img/hoc-logo.png';
+import HomeBg from 'assets/img/home-bg.png';
 
 export const WelcomeHeroWrapper = withTheme()(styled.div`
-    padding: 40px 0 40px;
+    padding-bottom: 40px;
     position: relative;
     margin-bottom: 50px;
 
-    &:after {
+    &:after, &:before {
         content: "";
         position: absolute;
         top: 0;
         left: 0;
         width: 100%;
         height: 100%;
-        background-color: ${props => props.theme.palette.primary.main};
-        opacity: .2;
+        background-image: url('${HomeBg}');
+        background-size: cover;
         z-index: -1;
+    }
+
+    &:after {
+      background-image: linear-gradient(270deg, rgba(0, 0, 0, 0) 0%, #000000 100%);
     }
 `);
 
 export const Logo = styled.div`
-    width: ${props => (props.width ? props.width : '100px')};
-    height: ${props => (props.height ? props.height : '100px')};
+    width: ${props => (props.width ? props.width : '60px')};
+    height: ${props => (props.height ? props.height : '60px')};
     background-image: url('${LogoImage}');
     background-size: contain;
     margin-right: 10px;
