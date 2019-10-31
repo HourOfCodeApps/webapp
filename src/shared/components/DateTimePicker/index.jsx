cc83a741ecfa2394412826c9c075ed1b47d818d5
Oleksandr Pidlisnyi <oleksandr.pidlisnyi@gmail.com>
2019-10-31 12:00:11 +0200

    HOC-9: localized buttons in timepicker (#64)

--- src/shared/components/DateTimePicker/index.jsx
+++ src/shared/components/DateTimePicker/index.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import MuiPickersUtilsProvider from 'material-ui-pickers/utils/MuiPickersUtilsProvider';
-// pick utils
 import LuxonUtils from 'material-ui-pickers/utils/luxon-utils';
-import { DateTimePicker as Picker } from 'material-ui-pickers';
-
+import { DateTimePicker as MuiDateTimePicker } from 'material-ui-pickers';
 
 const DateTimePicker = ({ value, ...props }) => (
   <MuiPickersUtilsProvider utils={LuxonUtils}>
-    <Picker
+    <MuiDateTimePicker
       clearable
       ampm={false}
       variant="outlined"
       value={value || null}
+      cancelLabel="Відміна"
+      okLabel="Підтвердити"
       {...props}
     />
   </MuiPickersUtilsProvider>
@@ -22,11 +22,13 @@ DateTimePicker.propTypes = {
   label: PropTypes.string,
   onChange: PropTypes.func.isRequired,
   value: PropTypes.instanceOf(Date),
+  clearable: PropTypes.bool,
 };
 
 DateTimePicker.defaultProps = {
   label: '',
   value: null,
+  clearable: false,
 };
 
 export default DateTimePicker;
