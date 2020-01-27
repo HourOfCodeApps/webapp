c33f94f834fe69e5258d92e51e00e3cf7a1190eb
Oleksandr Pidlisnyi <oleksandr.pidlisnyi@gmail.com>
2020-01-27 17:41:36 +0200

    HOC-38: configuration (#77)
    * HOC-38: switched to .env
    
    * HOC-38: added .env.example

--- internals/webpack.resolve.js
+++ internals/webpack.resolve.js
@@ -1,12 +1,9 @@
-const path = require('path');
+// const path = require('path');
 
 const dirs = require('./dirs');
 
 module.exports = {
   resolve: {
-    alias: {
-      config: path.resolve(dirs.src, 'config', process.env.NODE_ENV === 'production' ? 'production.js' : 'development.js'),
-    },
     extensions: ['.js', '.jsx'],
     modules: [dirs.src, 'node_modules'],
   },
